f4de9a414626f68a079e09aef4249788c5568be3
Kovanej <kovarmatej@gmail.com>
2022-04-27 12:48:09 +0200

    WIP adding grains distance & germs "distance" for boolean model with circles

--- Processes/particle_process.py
+++ Processes/particle_process.py
@@ -1,6 +1,9 @@
 
 import matplotlib.pyplot as plt
 from typing import List
+from itertools import product
+import numpy as np
+from sklearn.metrics import pairwise_distances
 
 from Geometry.particle import Particle
 
@@ -9,9 +12,39 @@ class ParticleProcess(object):
 
     def __init__(
             self,
-            particles: List[Particle]
+            particles: List[Particle],
+            grain_type: str
     ):
         self.particles = particles
+        self.number_of_particles = len(self.particles)
+        self.grain_type = grain_type
+        self.grains_distance_matrix = None
+        self.particles_distance_matrix = self._compute_the_particles_distance_matrix()
+        self.intersection_matrix = self._compute_the_intersection_matrix()
+
+    def _compute_the_particles_distance_matrix(self):
+        # TODO now works only for circles - otherwise return 0 matrix
+        distance_matrix = np.array([
+            [0 for i in range(self.number_of_particles)] for j in range(self.number_of_particles)
+        ])
+        if self.grain_type == "circle":
+            self.grains_distance_matrix = pairwise_distances(
+                [self.particles[k].germ for k in range(self.number_of_particles)]
+            )
+            radii_to_subtract = np.array([
+                [
+                    self.particles[k].grain.radius + self.particles[j].grain.radius
+                    for j in range(self.number_of_particles)
+                ] for k in range(self.number_of_particles)
+            ])
+            distance_matrix = np.where(
+                self.grains_distance_matrix < radii_to_subtract, 0, self.grains_distance_matrix - radii_to_subtract
+            )
+        return distance_matrix
+
+
+    def _compute_the_intersection_matrix(self):
+        pass
 
     def plot_itself(self, show_germs: bool = True):
         fig = plt.figure()
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from Geometry.particle import Particle
 from Processes.particle_process import ParticleProcess
 from Processes.point_process import PoissonPointProcess
 
-POISSON_INTENSITY = 2
+POISSON_INTENSITY = 8
 MARKED = False
 # everything right now for 2d
 SPACE_DIMENSION = 2
@@ -29,7 +29,7 @@ if __name__ == '__main__':
                 (MAX_CIRC_RAD - MIN_CIRC_RAD) * (np.random.random(1)[0]) + MIN_CIRC_RAD)
         ) for k in range(len(poisson_point_process.points))
     ]
-    particle_process = ParticleProcess(particles=particles)
+    particle_process = ParticleProcess(particles=particles, grain_type=GRAIN_TYPE)
     particle_process.plot_itself()
     a=1
 
