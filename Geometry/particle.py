04aafb8540a6a67b8d3867d94a93909c1e7185d1
Kovanej <kovarmatej@gmail.com>
2022-04-27 18:25:22 +0200

    WIP computing area & pairwise intersection area for circles in R^2

--- Geometry/particle.py
+++ Geometry/particle.py
@@ -11,10 +11,19 @@ class Particle(object):
             germ: Point,
             # TODO extend - now testing for circles
             grain: Circle,
+            grain_type: str = "circle",
             mark=None
     ):
         self.germ = germ
         self.grain = grain
         self.mark = mark
+        self.grain_type = grain_type
+        self.lebesgue_measure = self._compute_the_corresponding_measure()
+
+    def _compute_the_corresponding_measure(self):
+        if self.grain_type == "circle":
+            return self.grain.area()
+        else:
+            raise ValueError(f"Unknown value for Particle.grain_type: {self.grain_type}")
 
 
--- Processes/particle_process.py
+++ Processes/particle_process.py
@@ -18,19 +18,64 @@ class ParticleProcess(object):
         self.particles = particles
         self.number_of_particles = len(self.particles)
         self.grain_type = grain_type
-        self.grains_distance_matrix = None
+        # compute the grains distance
+        self.grains_distance_matrix = self._compute_the_grains_distance_matrix()
+        # compute particles distance (inf {||x_i - x_j||: x_i \in \Xi_i, x_j \in \Xi_j})
         self.particles_distance_matrix = self._compute_the_particles_distance_matrix()
-        self.intersection_matrix = np.where(self.particles_distance_matrix == 0, 1, 0)
+        # particles distance == 0 -> intersected, otherwise not intersected
+        self.particles_intersection_matrix = np.where(self.particles_distance_matrix == 0, 1, 0)
+        # compute the pairwise shared corresponding Lebesgue measure
+        # ("circle": shared areas, "segment": same as intersection matrix ...)
+        self.shared_corresponding_measure_matrix = self._compute_the_shared_corresponding_measure_matrix()
 
-    def _compute_the_particles_distance_matrix(self):
-        # TODO now works only for circles - otherwise return 0 matrix
-        distance_matrix = np.array([
-            [0 for i in range(self.number_of_particles)] for j in range(self.number_of_particles)
-        ])
+    def _compute_the_shared_corresponding_measure_matrix(self):
         if self.grain_type == "circle":
-            self.grains_distance_matrix = pairwise_distances(
+            shared_area_matrix = self._compute_the_pairwise_shared_areas_of_circles()
+            return shared_area_matrix
+        else:
+            raise ValueError(f"Unknown value for Particle.grain_type: {self.grain_type}")
+
+    def _compute_the_pairwise_shared_areas_of_circles(self):
+        # TODO subtract the area of circles outside the observational window
+        shared_areas_matrix = np.zeros(shape=self.particles_intersection_matrix.shape)
+        for i in range(self.number_of_particles):
+            for j in range(i, self.number_of_particles):
+                if i == j:
+                    shared_areas_matrix[i, j] = self.particles[i].lebesgue_measure
+                elif self.particles_intersection_matrix[i, j] == 0:
+                    shared_areas_matrix[i, j] = 0
+                    shared_areas_matrix[j, i] = 0
+                else:
+                    dist = self.grains_distance_matrix[i, j]
+                    rad_i_sq = self.particles[i].grain.radius ** 2
+                    rad_j_sq = self.particles[j].grain.radius ** 2
+                    x = (rad_i_sq - rad_j_sq + dist ** 2) / (2 * dist)
+                    z = x ** 2
+                    y = np.sqrt(rad_i_sq - z)
+
+                    if dist <= abs(self.particles[i].grain.radius - self.particles[j].grain.radius):
+                        shared_areas_matrix[i, j] = np.pi * min(rad_i_sq, rad_j_sq)
+                        shared_areas_matrix[j, i] = np.pi * min(rad_i_sq, rad_j_sq)
+                    else:
+                        # return a * asin(y / A.r) + b * asin(y / B.r) - y * (x + sqrt(z + b - a))
+                        _share = rad_i_sq * np.arcsin((y / self.particles[i].grain.radius)) + \
+                                 rad_j_sq * np.arcsin((y / self.particles[j].grain.radius)) - \
+                                 y * (x + np.sqrt(z + rad_j_sq - rad_i_sq))
+                        shared_areas_matrix[i, j] = _share
+                        shared_areas_matrix[j, i] = _share
+        return shared_areas_matrix
+
+    def _compute_the_grains_distance_matrix(self):
+        grains_distance_matrix = pairwise_distances(
                 [self.particles[k].germ for k in range(self.number_of_particles)]
             )
+        return grains_distance_matrix
+
+    def _compute_the_particles_distance_matrix(self):
+        # distance_matrix = np.array([
+        #     [0 for i in range(self.number_of_particles)] for j in range(self.number_of_particles)
+        # ])
+        if self.grain_type == "circle":
             radii_to_subtract = np.array([
                 [
                     self.particles[k].grain.radius + self.particles[j].grain.radius
@@ -40,7 +85,9 @@ class ParticleProcess(object):
             distance_matrix = np.where(
                 self.grains_distance_matrix < radii_to_subtract, 0, self.grains_distance_matrix - radii_to_subtract
             )
-        return distance_matrix
+            return distance_matrix
+        else:
+            raise ValueError(f"Unknown value for Particle.grain_type: {self.grain_type}")
 
     def plot_itself(self, show_germs: bool = True):
         fig = plt.figure()
