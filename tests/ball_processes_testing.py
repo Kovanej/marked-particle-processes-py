9fe53785ae0edd393cd716c8e3588aa5e08716dc
Kovanej <kovarmatej@gmail.com>
2022-05-05 16:02:13 +0200

    Update ball_processes_testing.py

--- tests/ball_processes_testing.py
+++ tests/ball_processes_testing.py
@@ -9,8 +9,8 @@ from Processes.ball_process import BallProcess
 import utils.const as const
 
 
-NO_OF_CIRCLES = 100
-NO_OF_INSIDE_CIRCLES = 20
+NO_OF_CIRCLES = 300
+NO_OF_INSIDE_CIRCLES = 400
 
 RANDOM_CENTERS = [
     (np.random.random_sample(), np.random.random_sample())
@@ -34,12 +34,27 @@ RANDOM_CENTERS = [
 # ] + \
 
 CENTERS_AND_RADII = [[
-    (Point([RANDOM_CENTERS[i][0], RANDOM_CENTERS[i][1]]), 1 / dv),
+    (Point([RANDOM_CENTERS[i][0], RANDOM_CENTERS[i][1]]), 1 / (dv + 1)),
     ] for dv in range(1, NO_OF_INSIDE_CIRCLES) for i in range(NO_OF_CIRCLES)
 ]
 
-CENTERS_AND_RADII = [c_r for inside_list in CENTERS_AND_RADII for c_r in inside_list]
 os.chdir("../")
+
+EPICENTER_COUNT = 1
+
+# CENTERS_AND_RADII = [[
+#     (Point([i / EPICENTER_COUNT, j / EPICENTER_COUNT]), 1 / (1 + np.sqrt(dv))),
+#     ] for dv in range(1, 1000) for i in range(1, 4) for j in range(1, EPICENTER_COUNT)
+# ]
+CENTERS_AND_RADII = [[
+    (Point([1/4, 1/3]), 1 / (dv)),
+    (Point([3/4, 1/3]), 1 / (dv)),
+    (Point([1/4, 2/3]), 1 / (dv)),
+    (Point([3/4, 2/3]), 1 / (dv)),
+    (Point([0.5, 1/3]), 1 / (dv)),
+    (Point([0.5, 2/3]), 1 / (dv)),
+] for dv in range(1, NO_OF_INSIDE_CIRCLES)]
+CENTERS_AND_RADII = [c_r for inside_list in CENTERS_AND_RADII for c_r in inside_list]
 
 particles = [
     Particle(
