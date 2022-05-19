700e8fbfd72690888b419d7157c0e165a964d4e1
Kovanej <kovarmatej@gmail.com>
2022-05-19 15:30:05 +0200

    update for merge

--- main.py
+++ main.py
@@ -25,8 +25,12 @@ if __name__ == '__main__':
         for k in range(len(poisson_point_process.points)):
             angle = np.pi / 4 * np.random.random(size=1)[0]
             length = (const.MAX_SEGMENT_LENGTH - const.MIN_SEGMENT_LENGTH) * np.random.random(size=1)[0] + const.MIN_SEGMENT_LENGTH
+            angle = np.pi * np.random.random_sample()
+            length = (const.MAX_SEGMENT_LENGTH - const.MIN_SEGMENT_LENGTH
+                      ) * np.random.random(size=1)[0] + const.MIN_SEGMENT_LENGTH
             mark_null_model = Mark(mark_type="discrete", mark_value=np.random.binomial(n=1, p=1/2, size=1)[0])
-            mark_angle_mark_model = Mark(mark_type="discrete", mark_value=np.random.binomial(n=1, p=angle / np.pi, size=1)[0])
+            mark_angle_mark_model = Mark(mark_type="discrete", mark_value=np.random.binomial(
+                n=1, p=angle / np.pi, size=1)[0])
             particles_null_model.append(
                 Particle(
                     germ=Point(poisson_point_process.points[k]),
@@ -35,34 +39,34 @@ if __name__ == '__main__':
                         end_point=Point(poisson_point_process.points[k]) + [np.cos(angle) * length / 2, np.sin(angle) * length / 2]
                     ),
                     grain_type="segment",
-                    # mark=mark_null_model
+                    mark=mark_null_model
                 )
             )
-            # particles_angle_mark_model.append(
-            #     Particle(
-            #         germ=Point(poisson_point_process.points[k]),
-            #         grain=Segment(
-            #             start_point=Point(poisson_point_process.points[k]) - [np.cos(angle) * length / 2,
-            #                                                                   np.sin(angle) * length / 2],
-            #             end_point=Point(poisson_point_process.points[k]) + [np.cos(angle) * length / 2,
-            #                                                                 np.sin(angle) * length / 2]
-            #         ),
-            #         grain_type="segment",
-            #         mark=mark_angle_mark_model
-            #     )
-            # )
+            particles_angle_mark_model.append(
+                Particle(
+                    germ=Point(poisson_point_process.points[k]),
+                    grain=Segment(
+                        start_point=Point(poisson_point_process.points[k]) - [np.cos(angle) * length / 2,
+                                                                              np.sin(angle) * length / 2],
+                        end_point=Point(poisson_point_process.points[k]) + [np.cos(angle) * length / 2,
+                                                                            np.sin(angle) * length / 2]
+                    ),
+                    grain_type="segment",
+                    mark=mark_angle_mark_model
+                )
+            )
 
     particle_process_null_model = SegmentProcess(
         particles=particles_null_model, germ_intensity=poisson_point_process.intensity
     )
-    # particle_process_angle_mark_model = SegmentProcess(
-    #     particles=particles_angle_mark_model, germ_intensity=poisson_point_process.intensity
-    # )
+    particle_process_angle_mark_model = SegmentProcess(
+        particles=particles_angle_mark_model, germ_intensity=poisson_point_process.intensity
+    )
 
-    # particle_process_null_model.compute_the_f_mark_characteristics()
-    # particle_process_angle_mark_model.compute_the_f_mark_characteristics()
+    particle_process_null_model.compute_the_f_mark_characteristics()
+    particle_process_angle_mark_model.compute_the_f_mark_characteristics()
 
     particle_process_null_model.plot_itself()
-    # particle_process_angle_mark_model.plot_itself()
+    particle_process_angle_mark_model.plot_itself()
     logging.info(f"{datetime.now()}: MAIN SCRIPT RUN ENDED")
     brkpnt = "breakpoint here"
