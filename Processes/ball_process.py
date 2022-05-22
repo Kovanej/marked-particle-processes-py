added particle measures + segment distance computation fix (memory consumption improvement)

Processes/ball_process.py
@@ -16,10 +16,13 @@ import utils.const as const
 # TODO extend ball processes to in R^d, current state is for d = 2
 class BallProcess(ParticleProcess):
 
-    def __init__(self, germ_intensity: float, particles: List[Particle]):
-        super().__init__(germ_intensity=germ_intensity, grain_type="ball", particles=particles, space_dimension=2)
+    def __init__(self, germ_intensity: float, particles: List[Particle], marked: bool = False):
+        self.radii_array = np.array([p.grain.radius for p in particles])
+        super().__init__(
+            germ_intensity=germ_intensity, grain_type="ball", particles=particles, space_dimension=2, marked=marked
+        )
 
-    def _compute_the_shared_corresponding_measure_matrix(self):
+    def _compute_the_pairwise_shared_measure_matrix(self):
         # TODO subtract the area of circles outside the observational window
         # TODO vectorize
         logging.info(f"{datetime.now()} :Circles shared measure matrix computation start.")
@@ -73,4 +76,9 @@ class BallProcess(ParticleProcess):
                 ax, facecolor=facecolor, linestyle="-", alpha=alpha, linewidth=1, edgecolor=edgecolor,
                 # alpha=0.5,
             )
+
+    def _compute_the_particles_measure(self):
+        # TODO currently for R^2 - improve with general formula later
+        areas = (self.radii_array ** 2) * np.pi
+        return areas
 

Processes/particle_process.py
@@ -20,7 +20,9 @@ class ParticleProcess(object):
             germ_intensity: float,
             particles: List[Particle],
             grain_type: str,
-            space_dimension: int
+            space_dimension: int,
+            marked: bool,
+            marks_aposteriori_type: Optional[str] = None
     ):
         logging.info(f"Particle process class initialized.")
         self.germ_intensity = germ_intensity
@@ -28,9 +30,12 @@ class ParticleProcess(object):
         self.number_of_particles = len(self.particles)
         self.grain_type = grain_type
         self.space_dimension = space_dimension
-        self.marks = np.array([p.mark.mark_value for p in self.particles])
-        self.marks_product = self.marks[..., None] * self.marks[None, ...]
-        self.marks_square = (self.marks[..., None] - self.marks[None, ...]) ** 2 / 2
+        self.marked = marked
+        if self.marked:
+            self.marks = np.array([p.mark.mark_value for p in self.particles])
+            self.marks_product = self.marks[..., None] * self.marks[None, ...]
+            self.marks_square = (self.marks[..., None] - self.marks[None, ...]) ** 2 / 2
+        self.particle_measure = self._compute_the_particles_measure()
         # compute the grains distance
         self.germs_distance_matrix = self._compute_the_germs_distance_matrix()
         # compute particles_null_model distance (inf {||x_i - x_j||: x_i \in \Xi_i, x_j \in \Xi_j})
@@ -39,11 +44,10 @@ class ParticleProcess(object):
         self.particles_intersection_matrix = self._compute_the_particles_intersection_matrix()
         # compute the pairwise shared corresponding Lebesgue measure
         # ("ball": shared areas, "segment": same as intersection matrix ...)
-        self.shared_corresponding_measure_matrix = self._compute_the_shared_corresponding_measure_matrix()
+        self.pairwise_shared_measure_matrix = self._compute_the_pairwise_shared_measure_matrix()
         # if needed following attributes are computed via executing ParticleProcess.compute_f_mark_statistics
         self.f_mark_normalization_constants: Dict[str, float] = {}
         self.f_mark_statistics: Dict[Tuple[str, str], float] = {}
-
 
     @staticmethod
     def _dot_pairwise(a, b):
@@ -59,7 +63,7 @@ class ParticleProcess(object):
         logging.info(f"{datetime.now()} :Particle intersection matrix computation end.")
         return intersection_matrix
 
-    def _compute_the_shared_corresponding_measure_matrix(self):
+    def _compute_the_pairwise_shared_measure_matrix(self):
         # overridden for each subclass - if not specified, it cannot be computed
         raise NotImplementedError(
             f"Method called from the instance of general ParticleProcess class. "
@@ -90,31 +94,43 @@ class ParticleProcess(object):
             ("product", "shared_area"): self._compute_the_f_mark_shared_area_correlation(f_type=f_type),
             ("square", "intersection"): self._compute_the_f_mark_intersection_correlation(f_type=f_type),
             ("square", "shared_area"): self._compute_the_f_mark_shared_area_correlation(f_type=f_type),
+            ("first_mark", "intersection"): self._compute_the_f_mark_intersection_correlation(f_type=f_type),
+            ("first_mark", "shared_area"): self._compute_the_f_mark_shared_area_correlation(f_type=f_type),
         }.get((f_type, weight_type))
         return f_mark_statistic
 
     def _compute_the_f_mark_shared_area_correlation(self, f_type: str):
-        shared_area_zero_diagonal = self.shared_corresponding_measure_matrix.copy()
-        shared_area_zero_diagonal[np.diag_indices(self.number_of_particles)] = 0
+        # since we are working on a [0, 1]^d window -> not normed by its Lebesgue measure, since it is 1 (je to jedno)
+        shared_area_zero_diagonal = self.pairwise_shared_measure_matrix.copy()
+        np.fill_diagonal(shared_area_zero_diagonal, 0)
         if f_type == "product":
             f_shared_nn = (self.marks_product * shared_area_zero_diagonal).sum() / 2
-            norm_by = (self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)) / 2
+            norm_by = self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)
         elif f_type == "square":
             f_shared_nn = (self.marks_square * shared_area_zero_diagonal).sum() / 2
-            norm_by = (self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)) / 2
+            norm_by = self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)
+        elif f_type == "first_mark":
+            shared_area_zero_on_and_under_diagonal = np.triu(shared_area_zero_diagonal)
+            f_shared_nn = (self.marks * shared_area_zero_on_and_under_diagonal).sum()
+            norm_by = self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)
         else:
             raise NotImplementedError(f"f-mark shared area correlation cannot be obtained for unknown f_type={f_type}")
         return f_shared_nn / norm_by
 
     def _compute_the_f_mark_intersection_correlation(self, f_type: str):
+        # since we are working on a [0, 1]^d window -> not normed by its Lebesgue measure, since it is 1 (je to jedno)
         intersection_zero_diagonal = self.particles_intersection_matrix.copy()
-        intersection_zero_diagonal[np.diag_indices(self.number_of_particles)] = 0
+        np.fill_diagonal(intersection_zero_diagonal, 0)
         if f_type == "product":
             f_intersection_nn = (self.marks_product * intersection_zero_diagonal).sum() / 2
-            norm_by = (self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)) / 2
+            norm_by = self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)
         elif f_type == "square":
             f_intersection_nn = (self.marks_square * intersection_zero_diagonal).sum() / 2
-            norm_by = (self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)) / 2
+            norm_by = self.f_mark_normalization_constants[f_type] * (self.germ_intensity ** 2)
+        elif f_type == "first_mark":
+            intersection_zero_on_and_under_diagonal = np.triu(intersection_zero_diagonal)
+            f_intersection_nn = (self.marks * intersection_zero_on_and_under_diagonal).sum()
+            norm_by = (self.f_mark_normalization_constants[f_type]) * (self.germ_intensity ** 2)
         else:
             raise NotImplementedError(f"f-mark intersection correlation cannot be obtained for unknown f_type={f_type}")
         self.f_intersection_nn = f_intersection_nn
@@ -125,13 +141,16 @@ class ParticleProcess(object):
         pairs_count = (self.number_of_particles * (self.number_of_particles - 1)) / 2
         if f_type == "product":
             marks_product_zero_diagonal = self.marks_product.copy()
-            marks_product_zero_diagonal[np.diag_indices(self.number_of_particles)] = 0
+            np.fill_diagonal(marks_product_zero_diagonal, 0)
             c_f_nn = marks_product_zero_diagonal.sum() / 2
             c_f = c_f_nn / pairs_count
         elif f_type == "square":
             # zero_diagonal by default - no need to "hardcode"
             c_f_nn = self.marks_square.sum() / 2
             c_f = c_f_nn / pairs_count
+        elif f_type == "first_mark":
+            c_f_nn = self.marks.sum()
+            c_f = c_f_nn / self.number_of_particles
         else:
             raise NotImplementedError(f"f-mark normalization constant cannot be obtained for unknown f_type={f_type}")
         self.f_mark_normalization_constants[f_type] = c_f
@@ -159,9 +178,10 @@ class ParticleProcess(object):
         )
 
     def _choose_edge_color(self):
-        return np.random.choice([
-                "#F9EFB4", "#F4BE9A", "#BA6191", "#40008C", "#000032"
-            ])
+        return "#ffffff"
+        # return np.random.choice([
+        #         "#F9EFB4", "#F4BE9A", "#BA6191", "#40008C", "#000032"
+        #     ])
 
     def _choose_face_color(self, particle=None):
         # TODO later adjust for marks
@@ -171,3 +191,6 @@ class ParticleProcess(object):
 
     def _choose_germ_color(self):
         return "#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)]), np.random.random(1)[0]
+
+    def _compute_the_particles_measure(self):
+        pass

Processes/point_process.py
@@ -65,10 +65,10 @@ class PoissonPointProcess(PointProcess):
         _number_of_points = np.random.poisson(
             lam=self.intensity * (self.window_edge_end_point - self.window_edge_start_point) ** self.space_dimension
         )
-        _pts = [
+        _pts = np.array([
             [np.random.random_sample() * (
                     self.window_edge_end_point - self.window_edge_start_point
             ) + self.window_edge_start_point for _ in range(self.space_dimension)] for _ in range(_number_of_points)
-        ]
+        ]).astype(np.float32)
         return Points(_pts)
 

Processes/segment_process.py
@@ -14,10 +14,13 @@ import utils.const as const
 
 class SegmentProcess(ParticleProcess):
 
-    def __init__(self, germ_intensity: float, particles: List[Particle], space_dimension: int = 2):
+    def __init__(
+            self, germ_intensity: float, particles: List[Particle], marked: bool = False, space_dimension: int = 2
+    ):
         logging.info(f"{datetime.now()} Segment process init start.")
         super().__init__(
-            germ_intensity=germ_intensity, grain_type="segment", particles=particles, space_dimension=space_dimension
+            germ_intensity=germ_intensity, grain_type="segment", particles=particles, space_dimension=space_dimension,
+            marked=marked
         )
         logging.info(f"{datetime.now()} Segment process angles matrix computation start.")
         self.angles_matrix = self._compute_the_angles_matrix()
@@ -33,7 +36,7 @@ class SegmentProcess(ParticleProcess):
         )
         return angles_matrix
 
-    def _compute_the_shared_corresponding_measure_matrix(self):
+    def _compute_the_pairwise_shared_measure_matrix(self):
         # TODO this generally doesn't hold for segments on the same line - fix
         logging.info(f"{datetime.now()} :Segments shared measure matrix computation start.")
         shared_measure_matrix = self.particles_intersection_matrix
@@ -99,31 +102,31 @@ class SegmentProcess(ParticleProcess):
         # TODO this is a slow performance part for some reason - fix this
         possible_distances = np.array([
             self._segments_vectorized_pairwise_distance(
-                np.clip(solution[..., 0], 0, 1).T, np.clip(solution[..., 1], 0, 1).T, start_points_pre, end_points_pre
+                np.clip(solution[..., 0], 0, 1), np.clip(solution[..., 1], 0, 1), start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_0_solution[..., 0], 0, 1), np.clip(alpha_0_solution[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_0_solution[..., 0], 0, 1).T, np.clip(alpha_0_solution[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_1_solution[..., 0], 0, 1), np.clip(alpha_1_solution[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_1_solution[..., 0], 0, 1).T, np.clip(alpha_1_solution[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(beta_0_solution[..., 0], 0, 1), np.clip(beta_0_solution[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(beta_0_solution[..., 0], 0, 1).T, np.clip(beta_0_solution[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(beta_1_solution[..., 0], 0, 1), np.clip(beta_1_solution[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(beta_1_solution[..., 0], 0, 1).T, np.clip(beta_1_solution[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_0_beta_0[..., 0], 0, 1), np.clip(alpha_0_beta_0[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_0_beta_0[..., 0], 0, 1).T, np.clip(alpha_0_beta_0[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_0_beta_1[..., 0], 0, 1), np.clip(alpha_0_beta_1[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_0_beta_1[..., 0], 0, 1).T, np.clip(alpha_0_beta_1[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_1_beta_0[..., 0], 0, 1), np.clip(alpha_1_beta_0[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_1_beta_0[..., 0], 0, 1).T, np.clip(alpha_1_beta_0[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
             self._segments_vectorized_pairwise_distance(
-                np.clip(alpha_1_beta_1[..., 0], 0, 1), np.clip(alpha_1_beta_1[..., 1], 0, 1), start_points_pre, end_points_pre
+                np.clip(alpha_1_beta_1[..., 0], 0, 1).T, np.clip(alpha_1_beta_1[..., 1], 0, 1).T, start_points_pre, end_points_pre
             ),
         ]).T
         logging.info(f"{datetime.now()} DISTANCE - Possible distances computation end.")
@@ -137,11 +140,11 @@ class SegmentProcess(ParticleProcess):
     def _segments_vectorized_pairwise_distance(alpha, beta, start_points, end_points):
         alpha = alpha[..., None]
         beta = beta[..., None]
-        left = alpha * start_points[:, None, None, :] + (1 - alpha) * end_points[:, None, None, :]
-        right = beta * start_points[None, :, None, :] + (1 - beta) * end_points[None, :, None, :]
+        left = alpha * start_points[:, None, :] + (1 - alpha) * end_points[:, None, :]
+        right = beta * start_points[None, :, :] + (1 - beta) * end_points[None, :, :]
         res = left - right
-        final = np.sqrt(np.power(res, 2).sum(axis=-1)).min(axis=-1)
-        return final
+        final = np.sqrt(np.power(res, 2).sum(axis=-1))
+        return final.T
 
     @staticmethod
     def _return_convex_combination(start_point: Point, end_point: Point, alpha: float):
@@ -154,9 +157,11 @@ class SegmentProcess(ParticleProcess):
             # col, alpha = self._choose_face_color()
             if particle.mark is not None:
                 if particle.mark.mark_value == 0:
-                    col, alpha = "#003271", 1
+                    col, alpha = "#0075C2", 1
                 elif particle.mark.mark_value == 1:
-                    col, alpha = "#FEC500", 1
+                    col, alpha = "#F87203", 1
+                else:
+                    col, alpha = np.random.choice(const.PARTICLE_COLORS_CHOICE), 1
             else:
                 col, alpha = np.random.choice(const.PARTICLE_COLORS_CHOICE), 1
             # alpha = Vector(particle.grain.start_point).norm() / np.sqrt(2)
@@ -170,3 +175,8 @@ class SegmentProcess(ParticleProcess):
                     ax_3d=ax, point=particle.grain.start_point,
                     #  edgecolor=col, alpha=alpha
                 )
+
+    def _compute_the_particles_measure(self):
+        vectors = np.array([p.grain.end_point - p.grain.start_point for p in self.particles])
+        lengths = self._norm(vectors)
+        return lengths

tests/f_mark_tests.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 from skspatial.objects import Circle, Point
 
 from Geometry.grain import Grain, Segment
@@ -13,29 +14,47 @@ from Processes.ball_process import BallProcess
 from Processes.point_process import PoissonPointProcess
 import utils.const as const
 
+
+os.chdir("../")
+
+
 TESTED_GRAIN_TYPE = "segment"
+TESTED_INTENSITY = 300
+MAX_SEGMENT_LENGTH = 0.4
+MIN_SEGMENT_LENGTH = 0.1
+MAX_CIRC_RAD = 0.1
+MIN_CIRC_RAD = 0.05
 
 
 logging.basicConfig(filename='example.log', filemode='w', level=logging.INFO)
 logging.info(f"{datetime.now()}: MAIN SCRIPT RUN STARTED & LOGGER INITIALIZED")
-poisson_point_process = PoissonPointProcess(intensity=const.POISSON_INTENSITY)
+poisson_point_process = PoissonPointProcess(intensity=TESTED_INTENSITY)
 if TESTED_GRAIN_TYPE == "segment":
     particles_null_model = []
     particles_angle_mark_model = []
     for k in range(len(poisson_point_process.points)):
         angle = np.pi * np.random.random_sample()
-        length = (
-                         const.MAX_SEGMENT_LENGTH - const.MIN_SEGMENT_LENGTH
-                  ) * np.random.random(size=1)[0] + const.MIN_SEGMENT_LENGTH
-        mark_null_model = Mark(mark_type="discrete", mark_value=np.random.binomial(n=1, p=1/2, size=1)[0])
-        mark_angle_mark_model = Mark(mark_type="discrete", mark_value=np.random.binomial(
-            n=1, p=angle / np.pi, size=1)[0])
+        length = (MAX_SEGMENT_LENGTH - MIN_SEGMENT_LENGTH) * np.random.random_sample() + MIN_SEGMENT_LENGTH
+        mark_null_model = Mark(
+            mark_type="discrete",
+            mark_value=np.random.random_sample()
+            # mark_value=np.random.binomial(n=1, p=1/2, size=1)[0]
+        )
+        mark_angle_mark_model = Mark(
+            mark_type="discrete",
+            mark_value=angle / np.pi
+            # mark_value=np.random.binomial(n=1, p=angle / np.pi, size=1)[0]
+        )
         particles_null_model.append(
             Particle(
                 germ=Point(poisson_point_process.points[k]),
                 grain=Segment(
-                    start_point=Point(poisson_point_process.points[k]) - [np.cos(angle) * length / 2, np.sin(angle) * length / 2],
-                    end_point=Point(poisson_point_process.points[k]) + [np.cos(angle) * length / 2, np.sin(angle) * length / 2]
+                    start_point=Point(
+                        poisson_point_process.points[k]
+                    ) - [np.cos(angle) * length / 2, np.sin(angle) * length / 2],
+                    end_point=Point(
+                        poisson_point_process.points[k]
+                    ) + [np.cos(angle) * length / 2, np.sin(angle) * length / 2]
                 ),
                 grain_type="segment",
                 mark=mark_null_model
@@ -56,24 +75,18 @@ if TESTED_GRAIN_TYPE == "segment":
         )
 
     particle_process_null_model = SegmentProcess(
-        particles=particles_null_model, germ_intensity=poisson_point_process.intensity
+        particles=particles_null_model, germ_intensity=poisson_point_process.intensity, marked=True
     )
-    particle_process_angle_mark_model = SegmentProcess(
-        particles=particles_angle_mark_model, germ_intensity=poisson_point_process.intensity
+    particle_process_non_null_model = SegmentProcess(
+        particles=particles_angle_mark_model, germ_intensity=poisson_point_process.intensity, marked=True
     )
 
-    particle_process_null_model.compute_the_f_mark_characteristics()
-    particle_process_angle_mark_model.compute_the_f_mark_characteristics()
-
-    particle_process_null_model.plot_itself()
-    particle_process_angle_mark_model.plot_itself()
-
-if TESTED_GRAIN_TYPE == "ball":
+elif TESTED_GRAIN_TYPE == "ball":
     particles_null_model = []
     particles_radius_mark_model = []
     for k in range(len(poisson_point_process.points)):
-        radius = np.random.random_sample() * (const.MAX_CIRC_RAD - const.MIN_CIRC_RAD) + const.MAX_CIRC_RAD
-        ind_mark = np.random.random_sample() * (const.MAX_CIRC_RAD - const.MIN_CIRC_RAD) + const.MAX_CIRC_RAD
+        radius = np.random.random_sample() * (MAX_CIRC_RAD - MIN_CIRC_RAD) + MAX_CIRC_RAD
+        ind_mark = np.random.random_sample() * (MAX_CIRC_RAD - MIN_CIRC_RAD) + MAX_CIRC_RAD
         mark_null_model = Mark(mark_type="continuous", mark_value=ind_mark)
         mark_radius_model = Mark(mark_type="continuous", mark_value=radius)
         particles_null_model.append(
@@ -94,16 +107,25 @@ if TESTED_GRAIN_TYPE == "ball":
         )
 
     particle_process_null_model = BallProcess(
-        germ_intensity=poisson_point_process.intensity, particles=particles_null_model
+        germ_intensity=poisson_point_process.intensity, particles=particles_null_model, marked=True
     )
-    particle_process_radius_model = BallProcess(
-        germ_intensity=poisson_point_process.intensity, particles=particles_radius_mark_model
+    particle_process_non_null_model = BallProcess(
+        germ_intensity=poisson_point_process.intensity, particles=particles_radius_mark_model, marked=True
     )
+else:
+    raise NotImplementedError()
 
-    particle_process_null_model.compute_the_f_mark_characteristics()
-    particle_process_radius_model.compute_the_f_mark_characteristics()
+particle_process_null_model.compute_the_f_mark_characteristics()
+particle_process_non_null_model.compute_the_f_mark_characteristics()
 
-    particle_process_null_model.plot_itself()
-    particle_process_radius_model.plot_itself()
+particle_process_null_model.plot_itself()
+particle_process_non_null_model.plot_itself()
 
 brkpnt = "breakpoint here"
+
+print(f"NULL MODEL VALUES:")
+for _, __ in particle_process_null_model.f_mark_statistics.items():
+    print(_, __)
+print(f"NON-NULL MODEL VALUES:")
+for _, __ in particle_process_non_null_model.f_mark_statistics.items():
+    print(_, __)