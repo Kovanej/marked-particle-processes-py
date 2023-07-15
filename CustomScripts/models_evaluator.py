5ad1c16ce6b39436c3be8abad127de05c5e6fdc5
Matěj Kovář <kovarmatej@gmail.com>
2023-07-15 19:52:37 +0200

    storing incorrect method

--- CustomScripts/models_evaluator.py
+++ CustomScripts/models_evaluator.py
@@ -12,9 +12,11 @@ import utils.const as const
 from utils.results_saver import ResultSaver
 
 
+# set up parameters which dataset to create
 model = 'ball_continuous_radius_alpha=0'
-f_type = 'product'
-w_type = 'intersection'
+f_type = 'first_mark'
+w_type = 'shared_area'
+
 
 with open("../config_models.json", "r") as json_data:
     config_json = json.loads(json_data.read())
@@ -33,6 +35,8 @@ for _ in range(envelope_count):
     result_saver = config_parser.return_the_result_saver(seed=seed)
     df_values = result_saver.results_all_df[['Input Value', 'PWFCF Value']]
     df_to_evaluate = pd.merge(completed_envelopes, df_values, on=['Input Value'])
+    df_to_evaluate = df_to_evaluate.loc[df_to_evaluate['Input Value'] > 0.04]
+    df_to_evaluate.plot(x='Input Value', y=['PWFCF Value', 'PWFCF Lower', 'PWFCF Upper'])
     this_shoud_be_zero = (df_to_evaluate['PWFCF Value'] > df_to_evaluate['PWFCF Upper']).sum() + (
                 df_to_evaluate['PWFCF Value'] < df_to_evaluate['PWFCF Lower']).sum()
     seed_inside_envelope[seed] = True if this_shoud_be_zero == 0 else False
