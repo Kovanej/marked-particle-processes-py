9028867618f6697991de3670651cc31e03b92de1
Matěj Kovář <kovarmatej@gmail.com>
2023-07-15 15:59:05 +0200

    envelopes final dataset output

--- CustomScripts/p_vals_outputer.py
+++ CustomScripts/p_vals_outputer.py
@@ -33,6 +33,19 @@ print(df_lower_file_path, df_upper_file_path)
 df_lower = pd.read_csv(df_lower_file_path)
 df_upper = pd.read_csv(df_upper_file_path)
 
-print(df_lower.head())
+df_lower = df_lower.loc[
+    (df_lower['f-Mark Type'] == f_type) & (df_lower['Weight Type'] == w_type) & (df_lower['Model'] == model)
+][['Model', 'f-Mark Type', 'Weight Type', 'Input Value', 'PWFCF Value']]
+df_lower.rename(columns={'PWFCF Value': 'PWFCF Lower'}, inplace=True)
 
+df_upper = df_upper.loc[
+    (df_upper['f-Mark Type'] == f_type) & (df_upper['Weight Type'] == w_type) & (df_upper['Model'] == model)
+][['Model', 'f-Mark Type', 'Weight Type', 'Input Value', 'PWFCF Value']]
+df_upper.rename(columns={'PWFCF Value': 'PWFCF Upper'}, inplace=True)
 
+df_model_envelope_values = merged_df = pd.merge(
+    df_lower, df_upper, on=['Model', 'f-Mark Type', 'Weight Type', 'Input Value']
+)
+
+df_model_envelope_values.to_csv(f"./completed_envelopes/df_envelope_values_{model}_{w_type}_{f_type}.csv", index=False)
+breakpoint_var=1
